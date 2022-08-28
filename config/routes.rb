5598a88e897dc3d9cecf7f2de08734f927e9c463
“shostitch“ <stitchsho0325@gmail.com>
2022-08-28 19:46:56 +0900

    中身作成開始

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'homes/top'
-  get 'homes/about'
+  root to: "homes#top"
+  get "about" => "homes#about", as: "about"
+  resources :users, except:[:new,:create]
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
